b037272b31f161d6108e935c44782d4d7a86ebc5
Kamran Ayub <kamran.ayub@gmail.com>
2014-05-20 23:04:57 -0500

    Setup resources

---
+++ Game/game/Resources.ts
@@ -0,0 +1,13 @@
+﻿var Resources = {
+   
+   //
+   // Textures
+   //
+   TextureBackground: new ex.Texture("images/background.gif"),
+   TextureMountains: new ex.Texture("images/mountains.png"),
+   TextureMoon: new ex.Texture("images/moon.png"),
+   TextureTree1: new ex.Texture("images/tree-1.gif"),
+   TextureLogo: new ex.Texture("images/logo.png"),
+   TextureFence: new ex.Texture("images/fence.gif")
+
+} 
--- Game/game/game.ts
+++ Game/game/game.ts
@@ -4,48 +4,59 @@
 /// <reference path="Player.ts" />
 /// <reference path="VictoryScene.ts" />
 /// <reference path="DefeatScene.ts" />
+/// <reference path="Resources.ts"/>
 
 var logger = ex.Logger.getInstance();
 
-var game = new ex.Engine(1000, 460, "game");
+var game = new ex.Engine(960, 600, "game");
 game.setAntialiasing(false);
 game.backgroundColor = ex.Color.Black;
 
 var loader = new ex.Loader();
 
-var ichabod = new Player(900, 300, ex.Color.Blue);
+for (var key in Resources) {
+   if (Resources.hasOwnProperty(key)) {
+      loader.addResource(Resources[key]);
+   }
+}
 
-var horseman = new Enemy(1100, 300, ex.Color.Red);
-game.addChild(horseman);
+game.start(loader).then(() => {
+   logger.info("All Resources have finished loading");
 
-var floor = new ex.Actor(200, 350, 10000, 10, ex.Color.Green);
-floor.collisionType = ex.CollisionType.Fixed;
-game.addChild(floor);
+   // todo
+   // bg
+   // moon
+   // mountains
+   // fence
+   // ground
 
-var testRock1 = new Obstacle(700, 350, ex.Color.Orange);
-testRock1.collisionType = ex.CollisionType.Fixed;
-game.addChild(testRock1);
+   var ichabod = new Player(900, 300, ex.Color.Blue);
 
-var testRock2 = new Obstacle(500, 350, ex.Color.Orange);
-testRock2.collisionType = ex.CollisionType.Fixed;
-game.addChild(testRock2);
+   var horseman = new Enemy(1100, 300, ex.Color.Red);
+   game.addChild(horseman);
 
-var testRock3 = new Obstacle(200, 350, ex.Color.Orange);
-testRock3.collisionType = ex.CollisionType.Fixed;
-game.addChild(testRock3);
+   var floor = new ex.Actor(200, 350, 10000, 10, ex.Color.Green);
+   floor.collisionType = ex.CollisionType.Fixed;
+   game.addChild(floor);
 
-var camera = new ex.SideCamera(game);
-camera.setActorToFollow(ichabod);
-game.camera = camera;
+   var testRock1 = new Obstacle(700, 350, ex.Color.Orange);
+   testRock1.collisionType = ex.CollisionType.Fixed;
+   game.addChild(testRock1);
 
-game.addChild(ichabod);
+   var testRock2 = new Obstacle(500, 350, ex.Color.Orange);
+   testRock2.collisionType = ex.CollisionType.Fixed;
+   game.addChild(testRock2);
 
-game.start();
+   var testRock3 = new Obstacle(200, 350, ex.Color.Orange);
+   testRock3.collisionType = ex.CollisionType.Fixed;
+   game.addChild(testRock3);
 
+   var camera = new ex.SideCamera(game);
+   camera.setActorToFollow(ichabod);
+   game.camera = camera;
 
+   game.addChild(ichabod);
 
-//game.start(loader).then(() => {
-//   logger.info("All Resources have finished loading");
-game.addScene("victory", new VictoryScene());
-game.addScene("defeat", new DefeatScene());   
-//});
+   game.addScene("victory", new VictoryScene());
+   game.addScene("defeat", new DefeatScene());   
+});
