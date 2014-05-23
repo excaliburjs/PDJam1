c53b8a7d4a9367e0f0c7c9b92532dcecabb05807
Josh Edeen <josh.edeen@gmail.com>
2014-05-23 16:33:08 -0500

    cleaned up victory + defeat scenes, narrative

--- Game/game/DefeatScene.ts
+++ Game/game/DefeatScene.ts
@@ -10,19 +10,34 @@
    public onInitialize(engine: ex.Engine): void {
       super.onInitialize(engine);
 
-      game.camera = new ex.BaseCamera(engine);
-
-      var labelDefeat = new ex.Label("you lose", game.getWidth() / 2, game.getHeight() / 2, '50px Segoe UI');
-      labelDefeat.color = ex.Color.White;
-      this.addChild(labelDefeat);
-
-      var distance = new ex.Label("distance: " + this.distanceTravelled, game.getWidth() / 2, 300 + game.getHeight() / 2, '50px Segoe UI');
-      distance.color = ex.Color.White;
-      this.addChild(distance);
-
       var death = new ex.Actor(0, 0, engine.canvas.width, engine.canvas.height);
       game.addChild(death);
       game.camera.setActorToFollow(death);
+
+      game.camera = new ex.BaseCamera(engine);
+
+      var w = game.getWidth() / 2;
+
+      var labelDefeat = new ex.Label("Defeat", w, 100, '50px Architects Daughter');
+      labelDefeat.color = ex.Color.Red;
+      labelDefeat.textAlign = ex.TextAlign.Center;
+      this.addChild(labelDefeat);
+
+      var labelDefeatStory1 = new ex.Label("A rock rose up from the path, and Ichabod was thrown", 50, 200, '25px Architects Daughter');
+      labelDefeatStory1.color = ex.Color.White;
+      this.addChild(labelDefeatStory1);
+
+      var labelDefeatStory2 = new ex.Label("from the back of old Gunpowder. As he lay there in", 50, 240, '25px Architects Daughter');
+      labelDefeatStory2.color = ex.Color.White;
+      this.addChild(labelDefeatStory2);
+
+      var labelDefeatStory3 = new ex.Label("the brambles, he felt darkness overtake his senses.", 50, 280, '25px Architects Daughter');
+      labelDefeatStory3.color = ex.Color.White;
+      this.addChild(labelDefeatStory3);
+
+      //var distance = new ex.Label("distance: " + this.distanceTravelled, game.getWidth() / 2, 300 + game.getHeight() / 2, '50px Segoe UI');
+      //distance.color = ex.Color.White;
+      //this.addChild(distance);
 
       //this.on("keyup", (ev: ex.KeyUp) => {
       //   window.location.reload()
--- Game/game/VictoryScene.ts
+++ Game/game/VictoryScene.ts
@@ -9,12 +9,25 @@
 
       game.camera = new ex.BaseCamera(engine);
 
-      var labelVictory = new ex.Label("you have escaped from the headless horseman", game.getWidth() / 2, game.getHeight() / 2, '50px Segoe UI');
-      labelVictory.color = ex.Color.White;
-      this.addChild(labelVictory);
-
       var victory = new ex.Actor(0, 0, engine.canvas.width, engine.canvas.height);
       game.addChild(victory);
       game.camera.setActorToFollow(victory);
+
+      var labelVictory = new ex.Label("Victory", game.getWidth() / 2, 100, '50px Architects Daughter');
+      labelVictory.color = ex.Color.White;
+      labelVictory.textAlign = ex.TextAlign.Center;
+      this.addChild(labelVictory);
+
+      var labelVictoryStory1 = new ex.Label("Ichabod spurred old Gunpowder onward a final time.", 50, 200, '25px Architects Daughter');
+      labelVictoryStory1.color = ex.Color.White;
+      this.addChild(labelVictoryStory1);
+
+      var labelVictoryStory2 = new ex.Label("All at once, horse and rider clattered across the old bridge.", 50, 240, '25px Architects Daughter');
+      labelVictoryStory2.color = ex.Color.White;
+      this.addChild(labelVictoryStory2);
+
+      var labelVictoryStory3 = new ex.Label("For a moment, he imagined himself safe.", 50, 280, '25px Architects Daughter');
+      labelVictoryStory3.color = ex.Color.White;
+      this.addChild(labelVictoryStory3);
    }
 }
